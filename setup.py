5c74d7ea53f3ea62a7b92637334b5cc980ce7729
Jonas Preußinger <jonas.preussinger@fau.de>
2024-10-31 20:27:20 +0100

    make repo pip installable

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="math_quiz_package",
+    version="0.1",
+    packages=find_packages(),
+    author="Jonas",
+    author_email="jonas.preussinger@fau.de",
+    description='A simple math quiz game package.',
+    url='https://github.com/jnsprssngr/dsss_hw_2',
+        
+)
